22b28449d9da5db821a4a06c884799202d6d50d8
Matt Gilstrap <mattgilstrap@Matts-Mac-mini.local>
2022-11-25 16:58:37 -0500

    initial commit

---
+++ BasicCrud/Controllers/TodoItemsController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace BasicCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+
+            // Create default InMemory data for Development
+            await setDefaultData();
+
+
+        return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(Guid id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+          if (_context.TodoItems == null)
+          {
+              return Problem("Entity set 'TodoContext.TodoItems'  is null.");
+          }
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(Guid id)
+        {
+            if (_context.TodoItems == null)
+            {
+                return NotFound();
+            }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(Guid id)
+        {
+            return (_context.TodoItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+        private async Task setDefaultData()
+        {
+            //******** Just to create InMemory default data********
+            var defaultItems = new List<TodoItem>();
+            defaultItems.Add(new TodoItem
+            {
+                Name = "Walk the Dog",
+                IsComplete = true
+            });
+            defaultItems.Add(new TodoItem
+            {
+                Name = "Grocery Shopping",
+                IsComplete = false
+            });
+            defaultItems.Add(new TodoItem
+            {
+                Name = "Mow the Lawn",
+                IsComplete = true
+            });
+            defaultItems.Add(new TodoItem
+            {
+                Name = "Clean Kitchen",
+                IsComplete = true
+            });
+            defaultItems.Add(new TodoItem
+            {
+                Name = "Cook dinner",
+                IsComplete = false
+            });
+
+            var defaultDbItems = await _context.TodoItems.ToListAsync();
+            if (defaultDbItems.Count() == 0)
+            {
+                defaultItems.ForEach(todo =>
+                {
+                    _context.TodoItems.Add(todo);
+                });
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BasicCrud/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// This tells the app to allow Cross Origin Requests from our front end
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            // TODO: AllowAnyHeader is propbably dangerous, need to find the correct way to set the CORS policy
+            policy.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
